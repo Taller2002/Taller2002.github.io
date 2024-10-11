4b7e0fe303a2395d2a332617b6caba2ae0100fbd
Taller2002 <rosduarte007@gmail.com>
2024-10-11 13:19:14 -0600

    Add files via upload

--- procesar.php
+++ procesar.php
@@ -1,28 +1,13 @@
-
-?>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recibir los datos del formulario
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $mensaje = $_POST['mensaje'];
 
-    // Definir el correo destinatario
-    $destinatario = "paolagonzaleslobo87@gmail.com"; // Cambia esto por tu correo
+    // Procesar los datos, como enviarlos por correo
+    mail('paolagonzaleslobo87@gmail.com', 'Nuevo Mensaje de Contacto', "Nombre: $nombre\nEmail: $email\nMensaje:\n$mensaje");
 
-    // Asunto del correo
-    $asunto = "Nuevo Mensaje de Contacto";
-
-    // Cuerpo del mensaje
-    $cuerpo = "Nombre: $nombre\nCorreo: $email\nMensaje:\n$mensaje";
-
-    // Enviar el correo
-    if (mail($destinatario, $asunto, $cuerpo)) {
-        echo "¡Mensaje enviado exitosamente!";
-    } else {
-        echo "Hubo un problema al enviar el mensaje.";
-    }
+    echo "¡Mensaje enviado!";
 }
 ?>
-
 
